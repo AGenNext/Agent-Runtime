d1eebf16a320c655533e2c843ac4329b756c9836
Chinmay Panda <chinmay@openautonomyx.com>
2026-05-19 16:49:50 -0700

    Add SurrealDB runtime store

---
+++ runtime/state/surreal.py
@@ -0,0 +1,25 @@
+from surrealdb import Surreal
+
+
+class SurrealRuntimeStore:
+    def __init__(self, url: str, namespace: str, database: str):
+        self.url = url
+        self.namespace = namespace
+        self.database = database
+        self.client = Surreal(self.url)
+
+    async def connect(self):
+        await self.client.connect()
+        await self.client.use(self.namespace, self.database)
+
+    async def write_workflow_run(self, workflow_id: str, state: dict):
+        return await self.client.create(
+            f"workflow_run:{workflow_id}",
+            state,
+        )
+
+    async def write_action_event(self, action_id: str, payload: dict):
+        return await self.client.create(
+            f"action_log:{action_id}",
+            payload,
+        )
