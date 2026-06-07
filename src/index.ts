a7cc1e05c4bddde3db7f3b53cbdefc24e80985c8
Chinmay Panda <chinmay@openautonomyx.com>
2026-06-07 12:49:45 -0700

    Export Fabric policy store and replay

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from "./fabric/types.js";
+export * from "./fabric/policy.js";
 export * from "./fabric/runtime.js";
+export * from "./fabric/store.js";
+export * from "./fabric/replay.js";
