179e3d461babedb9c4d01da8d54273bc8d50e475
Chinmay Panda <chinmay@openautonomyx.com>
2026-06-07 17:40:47 -0700

    Export Fabric chain loop primitives

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export * from "./fabric/policy.js";
 export * from "./fabric/runtime.js";
 export * from "./fabric/store.js";
 export * from "./fabric/replay.js";
+export * from "./fabric/trust.js";
+export * from "./fabric/reconciliation.js";
+export * from "./fabric/chain.js";
